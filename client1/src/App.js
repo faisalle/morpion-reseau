668374932754cab590a677ead52bea15e248abdc
faisalle <faisal93.fc@gmail.com>
2022-05-17 20:01:03 +0200

    set-grid

--- client1/src/App.js
+++ client1/src/App.js
@@ -2,34 +2,50 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import {io} from 'socket.io-client'
 
-const chooseSquare = square =>{
-
-}
-
 const Grid = ({socket, logout}) => {
     const [grid, setGrid] = useState (
         ['','','','','','','','',''] )
+
+    useEffect(() => {
+        socket.on('played',({pos, form}) => {
+            var newGrid = grid.slice()
+            newGrid[pos]=form
+            setGrid(newGrid)
+        socket.on('winX',() => {
+            setGrid(['','','','','','','','',''])
+        })
+        socket.on('winO',() => {
+            setGrid(['','','','','','','','',''])
+        })
+        socket.on('draw',() => {
+            setGrid(['','','','','','','','',''])
+        })
+        
+        })
+        return () => socket.off('played')
+    }, [grid,socket])
+
     return (
         <div>
-        <h1 className='title'>TIK TAK TOE</h1>   
-        <div className='gridcontainer'>
-            <div className="block" onClick={() =>chooseSquare(0)}> {grid[0]}</div>
-            <div className="block" onClick={() =>chooseSquare(1)}> {grid[1]}</div>
-            <div className="block" onClick={() =>chooseSquare(2)}> {grid[2]}</div>
+            <h1 className='title'>TIK TAK TOE</h1>   
+            <div className='gridcontainer'>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 0)}> {grid[0]}</div>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 1)}> {grid[1]}</div>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 2)}> {grid[2]}</div>
+            </div>
+            <div className='gridcontainer'>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 3)}> {grid[3]}</div>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 4)}> {grid[4]}</div>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 5)}> {grid[5]}</div>
+            </div>
+            <div className='gridcontainer'>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 6)}> {grid[6]}</div>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 7)}> {grid[7]}</div>
+                <div className="block" onClick={() =>socket.emit('clickSquare', 8)}> {grid[8]}</div>
+            </div>
+            <button className="lbutton" onClick={() => socket.emit("quit")}>Quit</button>
         </div>
-        <div className='gridcontainer'>
-            <div className="block" onClick={() =>chooseSquare(0)}> {grid[0]}</div>
-            <div className="block" onClick={() =>chooseSquare(1)}> {grid[1]}</div>
-            <div className="block" onClick={() =>chooseSquare(2)}> {grid[2]}</div>
-        </div>
-        <div className='gridcontainer'>
-            <div className="block" onClick={() =>chooseSquare(0)}> {grid[0]}</div>
-            <div className="block" onClick={() =>chooseSquare(1)}> {grid[1]}</div>
-            <div className="block" onClick={() =>chooseSquare(2)}> {grid[2]}</div>
-        </div>
-        <button className="button" onClick={() => socket.emit("quit")}>Quit</button>
-        </div>
-    )
+    )  
 }
 
 const Loading = ({socket,waiting, logout}) =>{
@@ -62,19 +78,20 @@ function App() {
     const [logout, setLogout] = useState(false)
     const [gameStarted, setGameStarted] = useState(false)
 
+  
     useEffect(() => {
-        const socket = io('http://localhost:8080')
+        const socket = io('http://192.168.39.41:8080')
         socket.on("loading ...", () => {
             setLoading(true)
-            setWaiting('waiting for player............')
+            setWaiting('waiting for friend that you dont have...') 
             setLogout('Logout')
             setGameStarted(false)
         })
-        socket.on("game started",() => {
-            setGameStarted(true)
+        socket.on("logout",() => {
             setLoading(false)
         })
-        socket.on("logout",() => {
+        socket.on("game started",() => {
+            setGameStarted(true)
             setLoading(false)
         })
         socket.on("quit",() => {
--- server/app.js
+++ server/app.js
@@ -9,12 +9,15 @@ const { compileFunction } = require('vm');
 
 var client1 = false
 var client2 = false
+var player = 'X'
 
 const io = socketIo(server, {
     cors: {
-        origin:'http://localhost:3000'
+        origin:('http://192.168.39.41:3000')
     }
 })
+var grid = ['','','','','','','','','']
+
 
 io.on("connection", socket => {
     console.log("connection .............")
@@ -33,21 +36,92 @@ io.on("connection", socket => {
         }
 
         console.log(client1 ? true : false, client2 ? true : false)      
-        
     })
     socket.on("logout",() =>{
         client1=false
         socket.emit("logout")
+        console.log('logout')
+        grid = ['','','','','','','','','']
         console.log(client1 ? true : false, client2 ? true : false)      
     })
     socket.on("quit",() => {
         client1.emit("quit")
         client2.emit("quit")
+        grid = ['','','','','','','','','']
         client1=false 
         client2=false
+        player='X'
         console.log(client1 ? true : false, client2 ? true : false)      
     
     })
-    
+    socket.on("clickSquare",(pos) => {
+
+        if((grid[pos]==='X' || grid[pos]==='O')){
+          console.log('case taken ')
+          
+        }
+       else if (socket===client1 && player==='X'){
+            grid[pos] = 'X'
+            console.log(`client1 clicked`)
+            client1.emit('played',{pos,form:'X'})
+            client2.emit('played',{pos,form:'X'})
+            player='O'
+        }
+        else if(socket===client2 && player==='O') {
+            grid[pos] = 'O'
+            console.log(`client2 clicked`)
+            client1.emit('played',{pos,form:'O'})
+            client2.emit('played',{pos,form:'O'})
+            player='X'
+        }
+        if (( grid[0]==='O'||grid[0]==='X')&&(grid[pos]==='X' || grid[pos]==='O')&&
+            (grid[1]==='O'||grid[1]==='X')&& 
+            (grid[2]==='O'||grid[2]==='X')&& 
+            (grid[3]==='O'||grid[3]==='X')&& 
+            (grid[4]==='O'||grid[4]==='X')&& 
+            (grid[5]==='O'||grid[5]==='X')&& 
+            (grid[6]==='O'||grid[6]==='X')&& 
+            (grid[7]==='O'||grid[7]==='X')&&     
+            (grid[8]==='O'||grid[8]==='X')){
+            client1.emit("draw")
+            client2.emit('draw')
+            console.log('draw')
+            grid = ['','','','','','','','','']
+            player='X'
+            
+        }
+
+        else if((grid[0]==='X'&&grid[1]==='X'&&grid[2]==='X')||
+            (grid[3]==='X'&&grid[4]==='X'&&grid[5]==='X')||
+            (grid[6]==='X'&&grid[7]==='X'&&grid[8]==='X')||
+            (grid[0]==='X'&&grid[3]==='X'&&grid[6]==='X')||
+            (grid[1]==='X'&&grid[4]==='X'&&grid[7]==='X')||
+            (grid[2]==='X'&&grid[5]==='X'&&grid[8]==='X')||
+            (grid[0]==='X'&&grid[4]==='X'&&grid[8]==='X')||
+            (grid[2]==='X'&&grid[4]==='X'&&grid[6]==='X')){
+            client1.emit('winX')
+            client2.emit('winX') 
+            console.log('winX')
+            grid = ['','','','','','','','',''] 
+            player='X'
+            return
+        }
+        else if((grid[0]==='O'&&grid[1]==='O'&&grid[2]==='O')||
+            (grid[3]==='O'&&grid[4]==='O'&&grid[5]==='O')||
+            (grid[6]==='O'&&grid[7]==='O'&&grid[8]==='O')||
+            (grid[0]==='O'&&grid[3]==='O'&&grid[6]==='O')||
+            (grid[1]==='O'&&grid[4]==='O'&&grid[7]==='O')||
+            (grid[2]==='O'&&grid[5]==='O'&&grid[8]==='O')||
+            (grid[0]==='O'&&grid[4]==='O'&&grid[8]==='O')||
+            (grid[2]==='O'&&grid[4]==='O'&&grid[6]==='O')){
+            client1.emit('winO')
+            client2.emit('winO') 
+            console.log('winO')
+            grid = ['','','','','','','','','']
+            player='X'
+            
+
+        }
+    })
 } )
 server.listen(port,() => console.log(`Listening on port ${port}`));
